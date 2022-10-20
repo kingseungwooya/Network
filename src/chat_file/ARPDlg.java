d23b8da3946e8172236e559ae8bc5119a3368092
202002538 Leechaeyeon <90441461+202002538@users.noreply.github.com>
2022-10-20 19:28:37 +0900

    Update ARPDlg.java

--- src/chat_file/ARPDlg.java
+++ src/chat_file/ARPDlg.java
@@ -30,7 +30,7 @@ public class ARPDlg extends JFrame implements BaseLayer {
    public int nUpperLayerCount = 0;
    public String pLayerName = null;
    public BaseLayer p_UnderLayer = null;
-   public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+   public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>(); //전송과정에서 필요한 변수들 선언 및 기본값으로 초기화
 
    public static LayerManager m_LayerMgr = new LayerManager();
 
@@ -95,49 +95,49 @@ public class ARPDlg extends JFrame implements BaseLayer {
       m_LayerMgr.AddLayer(new ARPDlg("GUI"));
       m_LayerMgr.ConnectLayers(" NI ( *Ethernet ( *ARP ( *IP ( *TCP ( *GUI ) ) ) ) )");
       ///////////////////
-   }
+   } //레이어 매니저 사용해서 각 레이어를 등록하고 계층의 순서대로 등록
 
-   class setAddressListener implements ActionListener {
+   class setAddressListener implements ActionListener { //Setting에 해당하는 버튼Action 
       @Override
       public void actionPerformed(ActionEvent e) {
 
          if (e.getSource() == Setting_Button) {
 
-            if (Setting_Button.getText() == "Reset") {
-               srcMacAddress.setText("");
+            if (Setting_Button.getText() == "Reset") { //reset에 대한 신호를 받음
+               srcMacAddress.setText(""); //텍스트를 모두 공백으로 초기화
                srcIpAddress.setText("");
-               Setting_Button.setText("Setting");
-               srcMacAddress.setEnabled(true);
+               Setting_Button.setText("Setting"); //리셋버튼이 setting버튼으로 바뀜
+               srcMacAddress.setEnabled(true); //reset을 누름으로서 다시 src와 맥 주소를 적을 수 있게 확성화 함
                srcIpAddress.setEnabled(true);
             } else {
-               byte[] MacAddress = new byte[6];
+               byte[] MacAddress = new byte[6]; //ARP헤더에서의 MAC, IP주소의 길이
                byte[] IpAddress = new byte[4];
 
-               String srcMac = srcMacAddress.getText();
+               String srcMac = srcMacAddress.getText(); //사용자가 적은 주소를 얻어와서 저장
                String srcIP = srcIpAddress.getText();
-               System.out.println("srcMacAddress : " + srcMac);
+               System.out.println("srcMacAddress : " + srcMac); //출력
                System.out.println("srcIPAddress : " + srcIP);
 
                String[] byte_srcMac = srcMac.split("-");
                for (int i = 0; i < 6; i++) {
                   MacAddress[i] = (byte) Integer.parseInt(byte_srcMac[i], 16);
-               }
+               } //맥주소를 '-'기준으로 쪼갠 뒤 저장한다. 맥주소는 6바이트이므로 저장할 배열 길이도 6
 
                String[] byte_srcIp = srcIP.split("\\.");
                for (int i = 0; i < 4; i++) {
                   IpAddress[i] = (byte) Integer.parseInt(byte_srcIp[i]);
-               }
+               } //아이피주소를 '-'기준으로 쪼갠 뒤 저장한다. 아이피주소는 4바이트이므로 저장할 배열 길이도 4이다
 
                srcIPNumber = IpAddress;
                srcMacNumber = MacAddress;
                
-               String[] dstMac = {"ff","ff","ff","ff","ff","ff"};
+               String[] dstMac = {"ff","ff","ff","ff","ff","ff"}; //주소지 맥주소를 모르므로 일단 ff-ff-... -ff로 적어둔다
                byte[] dstMacAddress = new byte[6];
                
                for (int i = 0; i < 6; i++) {
                   dstMacAddress[i] = (byte) Integer.parseInt(dstMac[i], 16);
                }
-               
+               //레이어 매니저를 통해 레이어를 얻어온 후 각 레이어에 Src주소, 도착지의 Mac주소 등을 전달한다
                ((EthernetLayer)m_LayerMgr.GetLayer("Ethernet")).SetEnetSrcAddress(MacAddress);
                ((EthernetLayer)m_LayerMgr.GetLayer("Ethernet")).SetEnetDstAddress(dstMacAddress);
                
@@ -148,55 +148,55 @@ public class ARPDlg extends JFrame implements BaseLayer {
                
                ((NILayer) m_LayerMgr.GetLayer("NI")).SetAdapterNumber(adapterNumber);
 
-               Setting_Button.setText("Reset");
-               srcMacAddress.setEnabled(false);
+               Setting_Button.setText("Reset"); //setting을 마침으로서 다시 사용자가 reset할 수 있도록 버튼이 reset으로 바뀜
+               srcMacAddress.setEnabled(false); //setting이 끝났으므로 맥주소와 아이피주소에 대한 설정 비활성화
                srcIpAddress.setEnabled(false);
 
             }
          }
          // basic ARP 전송
          if (e.getSource() == dstIpSendButton) {
-            if (dstIpSendButton.getText() == "Send") {
+            if (dstIpSendButton.getText() == "Send") {//작성한 주소를 Send머튼이 눌릴 때 얻어옴
                String dstIP = dstIpWrite.getText();
-               cacheArea.append(dstIP);
-               cacheArea.append("  ??-??-??-??-??-??");
+               cacheArea.append(dstIP); 
+               cacheArea.append("  ??-??-??-??-??-??"); //아직 맥주소를 모르기 때문에 다음과 같이 append한다. 
                cacheArea.append("  Incomplete" + "\n");
-               byte[] dstIPAddress = new byte[4];
+               byte[] dstIPAddress = new byte[4]; //주소를 세팅할 때와 같이 '.'기준으로 끊어 저장.
                String[] byte_dstIP = dstIP.split("\\.");
-               for (int i = 0; i < 4; i++) {
+               for (int i = 0; i < 4; i++) { // 헤더에서의 dstIP주소 크기가 4바이트이므로 크기 4의 배열에 저장
                   dstIPAddress[i] = (byte) Integer.parseInt(byte_dstIP[i], 10);
                }
                dstIPNumber = dstIPAddress;
-               ((TCPLayer) m_LayerMgr.GetLayer("TCP")).ARPSend(srcIPNumber, dstIPNumber);
+               ((TCPLayer) m_LayerMgr.GetLayer("TCP")).ARPSend(srcIPNumber, dstIPNumber); //바로 아래 계층인 TCP레이어에 주소 넘겨줌
                
             }
          }
          // proxy ARP 전송
-         if (e.getSource() == proxyAddButton) {
+         if (e.getSource() == proxyAddButton) { //Proxy ARP Entry에 해당하는 Button Action
             //proxy Add 
             if (proxyAddButton.getText() == "Add") {
-               String proxyDevice = proxyDeviceWrite.getText();
+               String proxyDevice = proxyDeviceWrite.getText(); //사용자가 작성한 Device, Ip, Mac을 각각 텍스트박스에서 읽어옴
                String proxyIP = proxyIpWrite.getText();
                String proxyMac = proxyMacWrite.getText();
-               proxyArpArea.append("Interface0");
+               proxyArpArea.append("Interface0"); //proxyArpArea에 맥주소 및 아이피주소 나타나게 함
                proxyArpArea.append("  " + proxyIP);
                proxyArpArea.append("  " + proxyMac + "\n");
                
-               byte[] proxyInterfaceByte = new byte[1];
+               byte[] proxyInterfaceByte = new byte[1]; // IP, Interfase, Mac주소의 각 크기에 맞는 배열 생성
                byte[] proxyIpByte = new byte[4];
                byte[] proxyMacByte = new byte[6];
-               String[] ip_split = proxyIP.split("\\.");
-               for (int i = 0; i < 4; i++) {
+               String[] ip_split = proxyIP.split("\\."); // Ip주소는 '.'을 기준으로 1바이트씩 4자리가 있음
+               for (int i = 0; i < 4; i++) { //따라서 4번 끊어서 저장
                   proxyIpByte[i] = (byte) Integer.parseInt(ip_split[i], 10);
                }
                
-               String[] mac_split = proxyMac.split("-");
-               for (int i = 0; i < 6; i++) {
+               String[] mac_split = proxyMac.split("-"); // Ip주소는 '.'을 기준으로 1바이트씩 6자리가 있음
+               for (int i = 0; i < 6; i++) { //따라서 6번 끊어서 저장
                   proxyMacByte[i] = (byte) Integer.parseInt(mac_split[i], 16);
                }
                
-               proxyInterfaceByte[0] = (byte)Integer.parseInt("1");
-               ((ARPLayer)m_LayerMgr.GetLayer("ARP")).addProxyTable(proxyInterfaceByte, proxyIpByte, proxyMacByte);
+               proxyInterfaceByte[0] = (byte)Integer.parseInt("1"); //인터페이스 활성화
+               ((ARPLayer)m_LayerMgr.GetLayer("ARP")).addProxyTable(proxyInterfaceByte, proxyIpByte, proxyMacByte); //ARP레이어에 얻은 정보를 전달한다
             }
             //proxy Delete 
             else if(proxyAddButton.getText() == "Delete") {
@@ -205,19 +205,19 @@ public class ARPDlg extends JFrame implements BaseLayer {
          }
          
          //GARP 전송 
-         if (e.getSource() == GArpSendButton) {
-            String garp = GArpAddressWrite.getText();
+         if (e.getSource() == GArpSendButton) { //Gartuitous ARP에 해당하는 Button Action
+            String garp = GArpAddressWrite.getText(); // 입력한 주소를 읽어옴
             byte[] garpByte = new byte[6];
             
             String[] garp_split = garp.split("-");
             for (int i = 0; i < 6; i++) {
                garpByte[i] = (byte) Integer.parseInt(garp_split[i], 16);
             }
-            String[] dstMac = {"ff","ff","ff","ff","ff","ff"};
+            String[] dstMac = {"ff","ff","ff","ff","ff","ff"}; // ARP하기 전에는 맥주소를 모른다. 그래서 왼쪽과 같은 임시적 초기화
             byte[] dstMacAddress = new byte[6];
             
             for (int i = 0; i < 6; i++) {
-               dstMacAddress[i] = (byte) Integer.parseInt(dstMac[i], 16);
+               dstMacAddress[i] = (byte) Integer.parseInt(dstMac[i], 16);//주소 끊어서 저장하고 초기값 설정 과정, 위의 다른 함수들과 같음
             }
             
             //((EthernetLayer)m_LayerMgr.GetLayer("Ethernet")).SetEnetSrcAddress(garpByte);
@@ -227,44 +227,46 @@ public class ARPDlg extends JFrame implements BaseLayer {
             ((ARPLayer)m_LayerMgr.GetLayer("ARP")).SetArpDstAddress(dstMacAddress);
             
             ((ARPLayer)m_LayerMgr.GetLayer("ARP")).ARPSend(srcIPNumber, srcIPNumber);
-         }
+         }// 각 하위 레이어에서 필요로 할 정보를 전달한다.
+         
+         
          //Chatting send 
-         if (e.getSource() == Chat_send_Button) {
+         if (e.getSource() == Chat_send_Button) { // Chatting에 해당하는 Button Action
             if (Setting_Button.getText() == "Reset") {
                for (int i = 0; i < 10; i++) {
-                  String input = ChattingWrite.getText();
-                  ChattingArea.append("[SEND] : " + input + "\n");
+                  String input = ChattingWrite.getText(); //사용자가 입력한 텍스트 읽어옴
+                  ChattingArea.append("[SEND] : " + input + "\n"); //Send라는 문자열을 앞에 달아서 채팅화면에 출력
                   byte[] bytes = input.getBytes();
-                  m_LayerMgr.GetLayer("ChatApp").Send(bytes, bytes.length);
-                  if (m_LayerMgr.GetLayer("GUI").Receive()) {
+                  m_LayerMgr.GetLayer("ChatApp").Send(bytes, bytes.length); //채팅이므로 두가지 Application 계층 중 ChatApp에 전달
+                  if (m_LayerMgr.GetLayer("GUI").Receive()) { // 상대로부터 답신을 받음
                      input = Text;
-                     ChattingArea.append("[RECV] : " + input + "\n");
+                     ChattingArea.append("[RECV] : " + input + "\n"); //RECV라는 문자열을 앞에 달아서 채팅화면에 출력
                      continue;
                   }
                   break;
                }
             } else {
-               JOptionPane.showMessageDialog(null, "Address Configuration Error");
+               JOptionPane.showMessageDialog(null, "Address Configuration Error"); //주소 입력에 문제가 있는 경우
             }
          }
-         if (e.getSource() == openFileButton) {
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("txt", "txt");
-            JFileChooser chooser = new JFileChooser();
+         if (e.getSource() == openFileButton) { // file trasfer에 해당하는 Button Action
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("txt", "txt"); //txt확장자만 필터링 함
+            JFileChooser chooser = new JFileChooser(); 
             chooser.setFileFilter(filter);
-            int ret = chooser.showOpenDialog(null);
+            int ret = chooser.showOpenDialog(null); //보낼 파일 선택 
             if (ret == JFileChooser.APPROVE_OPTION) {
                String filePath = chooser.getSelectedFile().getPath();
                fileArea.setText(filePath);
-               File_send_Button.setEnabled(true);
+               File_send_Button.setEnabled(true); //send버튼 활성화
                file = chooser.getSelectedFile();
 
             }
 
          }
          //File send 
-         if (e.getSource() == File_send_Button) {
+         if (e.getSource() == File_send_Button) { //FileApp layer로 파일을 보냄
             ((FileAppLayer) m_LayerMgr.GetLayer("FileApp")).setAndStartSendFile();
-            File_send_Button.setEnabled(false);
+            File_send_Button.setEnabled(false); //send버튼 비활성화 
          }
       }
 
@@ -391,7 +393,7 @@ public class ARPDlg extends JFrame implements BaseLayer {
       GArpAddress = new JLabel("H/W address");
       GArpAddress.setBounds(10, 30, 100, 10);
       GArpAddressWrite = new JTextField();
-      GArpAddressWrite.setBounds(20, 60, 220, 20); /////////////////@@@@@@@@@@@@@@@@
+      GArpAddressWrite.setBounds(20, 60, 220, 20); 
       GArpSendButton = new JButton("전송");
       GArpSendButton.setBounds(70, 100, 100, 30);
       gratuitousPanel.add(GArpAddress);
@@ -451,7 +453,7 @@ public class ARPDlg extends JFrame implements BaseLayer {
       NICComboBox.addActionListener(new ActionListener() { // Event Listener
 
          @Override
-         public void actionPerformed(ActionEvent e) {
+         public void actionPerformed(ActionEvent e) { //address setting panel에서 NIC콤보박스를 누르면 선택가능한 리스트를 보여준다
             // TODO Auto-generated method stub
 
             adapterNumber = NICComboBox.getSelectedIndex();
@@ -557,7 +559,7 @@ public class ARPDlg extends JFrame implements BaseLayer {
       return this.file;
    }
 
-   public String get_MacAddress(byte[] byte_MacAddress) {
+   public String get_MacAddress(byte[] byte_MacAddress) { //맥주소를 반환
 
       String MacAddress = "";
       for (int i = 0; i < 6; i++) {
@@ -571,7 +573,7 @@ public class ARPDlg extends JFrame implements BaseLayer {
       return MacAddress;
    }
 
-   public boolean Receive(byte[] input) {
+   public boolean Receive(byte[] input) { //받은 채팅 메시지를 채팅창에 출력 
       if (input != null) {
          byte[] data = input;
          Text = new String(data);
@@ -629,24 +631,24 @@ public class ARPDlg extends JFrame implements BaseLayer {
 
    // cache table setting
    // ip , ethernet , status(0,1)
-   public void setArpCache(ArrayList<ArrayList<byte[]>> cacheTable) {
+   public void setArpCache(ArrayList<ArrayList<byte[]>> cacheTable) { //캐시테이블 리스트를 전달받음
       this.cacheTable = cacheTable;
       cacheArea.setText("");
       //byte[] ipAddressByte = new byte[4];
       //byte[] macAddressByte = new byte[6];
       System.out.println("set arp cache");
 
-      for(int i=0; i<cacheTable.size(); i++) {
+      for(int i=0; i<cacheTable.size(); i++) { //모든 리스트 내 원소에 대해 
          byte[] ip_byte = cacheTable.get(i).get(0);
          byte[] mac_byte = cacheTable.get(i).get(1);
          byte[] status_byte = cacheTable.get(i).get(2);
          
-         String ipByte1 = Integer.toString(Byte.toUnsignedInt(ip_byte[0]));
+         String ipByte1 = Integer.toString(Byte.toUnsignedInt(ip_byte[0])); //ip주소 출력
          String ipByte2 = Integer.toString(Byte.toUnsignedInt(ip_byte[1]));
          String ipByte3 = Integer.toString(Byte.toUnsignedInt(ip_byte[2]));
          String ipByte4 = Integer.toString(Byte.toUnsignedInt(ip_byte[3]));
          
-         String macByte1 = Integer.toHexString(Byte.toUnsignedInt(mac_byte[0]));
+         String macByte1 = Integer.toHexString(Byte.toUnsignedInt(mac_byte[0])); //mac주소 출력
          String macByte2 = Integer.toHexString(Byte.toUnsignedInt(mac_byte[1]));
          String macByte3 = Integer.toHexString(Byte.toUnsignedInt(mac_byte[2]));
          String macByte4 = Integer.toHexString(Byte.toUnsignedInt(mac_byte[3]));
@@ -658,7 +660,7 @@ public class ARPDlg extends JFrame implements BaseLayer {
          System.out.println(ipByte1+"."+ipByte2+"."+ipByte3+"."+ipByte4);
          System.out.println("  "+macByte1+"-"+macByte2+"-"+macByte3+"-"+macByte4+"-"+macByte5+"-"+macByte6);
 
-         if (byte2ToInt(status_byte[0], status_byte[1])==1) {
+         if (byte2ToInt(status_byte[0], status_byte[1])==1) { //complete여부 출력
             cacheArea.append("  complete" + "\n");
          }
          else {
